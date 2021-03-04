75a2a30c6a009682963a3db2f2b60b37584950a3
ArunavaNag <arunavoster@gmail.com>
2021-03-03 17:39:10 -0800

    local processing updated to generate plot

--- data_exploration/process.py
+++ data_exploration/process.py
@@ -20,14 +20,15 @@ pd.TimeSeries = pd.Series
 from geopy import distance
 
 #plots
-import pylab as plt
+import pylab as pyplt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 #others
 import time
+import decimal
 
-def load_data():
+def load_dataframe():
   set_number = 5
   dir = '~/Documents/Myfiles/DataAnalysis/data/Sprints/Run03/Set0'+str(set_number)+'/'
   windbag = 'wind0'+str(set_number)+'Run03.hdf'
@@ -44,21 +45,35 @@ def load_data():
   return windn
 
 
-def get_position(df):
+def get_position(df, dt):
   # summation till Nth particle
-  eastwest = [np.sum(df.U[j:])*dt for j in range(0,len(df))]
-  northsouth = [np.sum(df.V[j:])*dt for j in range(0,len(df))]
+  eastwest = [np.sum(df.U[j:]) * dt for j in range(0,len(df))]
+  northsouth = [np.sum(df.V[j:]) * dt for j in range(0,len(df))]
   odor_position = np.array([[df.xsrc[i],df.ysrc[i]] for i in range (len(df))]) # odor particle
   
   return eastwest, northsouth, odor_position
 
-def create_wind_position_frame(windn, odor_position): 
+def update_frame(odor_presence, wind_data_frame):
+  df = wind_data_frame
+  #value is compared with encountered particle and concentration is copied into the existing data frame
+  odor_expected = []
+  for i in range(len(odor_presence)):
+    if(odor_presence[i]==1):
+        odor_expected.append(df.odor[i])
+    else:
+        odor_expected.append(0.6) #as per sensor reading anything below 1.5v
+  df['odor_expected'] = odor_expected
+
+  return df
+
+def calculate_expected_encounters(windn): 
   
   df = pd.DataFrame()
   df = windn
-  dt= df.master_time[1]-df.master_time[0]
+  dt = df.master_time[1]-df.master_time[0]
   odor_presence=[]
-  eastwest , northsouth , odor_position = get_position(df)
+  print('Getting Encounters')
+  eastwest , northsouth , odor_position = get_position(df, dt)
 
   l = len(df)
   for i in range((len(eastwest))-1, -1, -1):        # moving backwards
@@ -79,13 +94,51 @@ def create_wind_position_frame(windn, odor_position):
         odor_presence.append(1)
     else:
         odor_presence.append(0)
+  
+  print('Finishing Calculating Encounters')
 
   return odor_presence
 
-def main():
-  windn = load_data()
-  odor_presence = create_wind_position_frame(windn)
+def plot_time_series(df):
+  f, (ax1,ax2) = plt.subplots(2, 1,figsize=(20,10))
+  ax1.plot(df.sync_time, df.odor)
+  ax1.set_ylabel('Concentration', fontsize=20)
+  ax1.title.set_text('Encountered Particle')
+  ax2.plot(df.sync_time,df.odor_expected)
+  ax2.set_xlabel('Time (secs)', fontsize=20)
+  ax2.set_ylabel('Concentration', fontsize=20)
+  ax2.title.set_text('Calculated Particle')
+
+  f.suptitle('Plot - Radius time**0.5*0.01', fontsize =20)
+  f.show()
+  plt.show()
+
   
+
+def plot_concentration(df):
+  f1, (ax1,ax2) = plt.subplots(2, 1,figsize=(20,10))
+  ax1.scatter(df.sync_time, df.odor, c=df.odor, s=5, cmap='magma')
+  ax1.set_ylabel('Concentration', fontsize=20)
+  ax1.title.set_text('Encountered Particle')
+  ax2.scatter(df.sync_time,df.odor_expected, c=df.odor_expected, s=5, cmap='magma')
+  ax2.set_xlabel('Time (secs)', fontsize=20)
+  ax2.set_ylabel('Concentration', fontsize=20)
+  ax2.title.set_text('Calculated Particle')
+
+  f1.suptitle('Odor Oncentration - Radius time**0.5*0.01', fontsize =20)
+  f1.show()
+  plt.show()
+
+def main():
+  windn = load_dataframe()  #load wind data
+  print('\nComputing Wind Position')
+  odor_presence = calculate_expected_encounters(windn)
+  print('\nUpdating Wind Data Frame with Calculated Encounters')
+  updated_df = update_frame(odor_presence, windn) 
+  print('\nPlot Time Series')
+  plot_time_series(updated_df)
+  print('\nPlot Concentration')
+  plot_concentration(updated_df)
 
 if __name__ == "__main__":
   # execute only if run as a script
