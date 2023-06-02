a9ddd7eb83a7bc147bf6a33b1a54b1234b33da5e
Arunava Nag <arunavoster@gmail.com>
2023-06-02 16:58:28 -0700

    added more multiprocessing modules

---
+++ data_exploration/lpfAnalysisLocal.py
@@ -0,0 +1,187 @@
+# user defined functions
+import odor_statistics_lib as osm
+
+# dataframes
+import pandas as pd
+import h5py
+
+#suppress warnings
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+pd.TimeSeries = pd.Series 
+
+#math
+import numpy as np
+import math
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from scipy import signal
+import scipy 
+import scipy.stats as st
+from scipy.stats import kurtosis
+
+#plots
+import string
+import figurefirst
+from figurefirst import FigureLayout,mpl_functions
+import matplotlib.ticker as mtick
+import pylab as plt
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
+from mpl_toolkits.axes_grid1 import make_axes_locatable # for colorbar
+import seaborn as sns
+from sklearn.inspection import DecisionBoundaryDisplay
+from matplotlib.colors import ListedColormap
+sns.set_style("whitegrid")
+pd.options.mode.chained_assignment = None
+
+from multiprocessing import Pool
+import multiprocessing as mp
+from functools import partial
+
+dir = '~/DataAnalysis/data/Sprints/HighRes/'
+
+#suppress warnings
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+pd.TimeSeries = pd.Series 
+
+def get_timed_rows(dataframe,duration_of_encounters):
+    x = dataframe.sample(1)
+    A = x.mean_time.values.round(0) - duration_of_encounters
+    B = x.mean_time.values.round(0)
+    timed_rows = dataframe.loc[(dataframe.mean_time > A[0]) & (dataframe.mean_time < B[0])]
+    return timed_rows
+    
+def get_timed_encounter_stats(dataframe, distance_class, duration_of_encounters):
+    df_q = dataframe.query('type == ' + str(distance_class))   
+    df_q.reset_index(inplace=True, drop=True)     
+            
+    Nrows = get_timed_rows(df_q,duration_of_encounters)
+    avg_dist = np.mean(Nrows.avg_dist_from_source)
+    mean_time_whiff=np.mean(Nrows.mean_time)
+#     mean_conc=np.mean(Nrows_cont.odor)
+    pack_data=np.vstack([Nrows.mean_concentration,Nrows.mean_ef,Nrows.log_whiff,Nrows.whiff_ma,Nrows.std_whiff])
+    return pack_data,avg_dist,len(Nrows),mean_time_whiff
+
+
+def gather_stat_timed(dataframe, distance_class, duration_of_encounters,X,y,D,N,T):
+    for i in range(500):
+        xx,dx,n,t=get_timed_encounter_stats(dataframe,
+                                               distance_class, duration_of_encounters)
+        X.append(xx)
+        D.append(dx)
+        y.append(distance_class)
+        N.append(n)
+        T.append(t)
+        
+    return X,y,D,N,T
+
+
+def create_class_column_fullts(dataframe):
+    dataframe.loc[dataframe.distance_from_source < 5, 'type'] = 0
+    dataframe.loc[(dataframe.distance_from_source >= 5)  & (dataframe.distance_from_source < 30), 'type'] = 1
+    dataframe.loc[dataframe.distance_from_source >= 30, 'type'] = 2
+    return dataframe
+
+def create_class_column(dataframe):
+    dataframe.loc[dataframe.avg_dist_from_source < 5, 'type'] = 0
+    dataframe.loc[(dataframe.avg_dist_from_source >= 5)  & (dataframe.avg_dist_from_source < 30), 'type'] = 1
+    dataframe.loc[dataframe.avg_dist_from_source >= 30, 'type'] = 2
+    return dataframe
+
+
+def get_statistics(df,index,fdf):
+    osm.avg_distance(df,index,fdf)
+    osm.mean_conc(df,index,fdf)
+    osm.motion_statistics(df,index,fdf)
+    osm.whiff_blank_duration(df,index,fdf)
+    osm.trajectory_speed(df,index,fdf)
+    osm.encounter_frequency(df,index,fdf,1,2)
+    osm.mean_avg(df,index,fdf)
+    osm.mean_t(df,index,fdf)
+    osm.std_whiff(df,index,fdf)
+
+def calc_val(X):
+    return np.ravel([np.min(X),np.max(X),np.mean(X),np.std(X),kurtosis(X)])
+
+## load dataframes
+df_windy=pd.read_hdf(dir+'Windy/WindyMASigned.h5')
+df_notwindy=pd.read_hdf(dir+'NotWindy/NotWindyMASigned.h5')
+df_forest=pd.read_hdf(dir+'Forest/ForestMASigned.h5')
+
+## combine dataframes
+# desert = pd.concat([df_notwindy,df_windy,df_forest])
+# desert.reset_index(inplace=True, drop=True)
+
+def process(cutoff_freq):
+
+    trainset= df_notwindy.copy()
+
+    ## low pass filtering
+    sos = signal.butter(2, cutoff_freq, 'low',fs=200, output='sos')
+    filtered = signal.sosfilt(sos, trainset.odor)
+    dt = trainset.master_time[1]-trainset.master_time[0]
+    time = []
+    time.append(0)
+    for i in range(1,len(trainset)):
+        time.append(time[i-1]+dt)
+    trainset['time'] = time
+    trainset['odor']=filtered
+
+    ## calulcate statistics
+    np.seterr(divide = 'ignore') 
+    index = osm.get_index(trainset)
+    fdf = pd.DataFrame()
+    get_statistics(trainset,index,fdf)
+    stat_cal_set=create_class_column(fdf)
+
+    ## calculate statistics
+    D_train=[]
+    mean_time_train=[]
+
+    Xtrain = []
+    ytrain = []
+    Nrows_train = []
+
+    for distance_class in [0,1,2]:
+        Xtrain, ytrain, D_train,Nrows_train,mean_time_train = gather_stat_timed(stat_cal_set,distance_class,
+                      lookback_time, Xtrain,ytrain,D_train,Nrows_train,
+                      mean_time_train)
+    column_names=['mc_min','mc_max','mc_mean','mc_std_dev','mc_k',
+                 'wf_min','wf_max','wf_mean','wf_std_dev','wf_k',
+                 'wd_min','wd_max','wd_mean','wd_std_dev','wd_k',
+                 'ma_min','ma_max','ma_mean','ma_std_dev','ma_k',
+                 'st_min','st_max','st_mean','st_std_dev','st_k']
+
+    traindf=pd.DataFrame(columns = column_names)
+    c1=[]
+    for i in range(len(Xtrain)):
+        if(np.size(Xtrain[i])==0):
+            c1.append(i)
+            continue
+        else:
+            X=[]
+            for j in range(len(Xtrain[i])):
+                X.append(calc_val(Xtrain[i][j]))
+            traindf.loc[i]=np.ravel(X)
+
+    traindf['distance']=np.delete(D_train, c1)
+    
+    distance=smf.ols(formula=""" distance ~ mc_mean + ma_std_dev + ma_max + wd_std_dev""", data=traindf).fit()
+
+    return distance.rsquared    
+
+if __name__ == "__main__":
+    lookback_time = 10
+    filters=[2,4,6,8,10,15,20,40,60,70,80,90]
+
+    with Pool(mp.cpu_count()-4) as pool:
+        lpf = pool.map(process, filters)
+
+    print('done')
+    lpdf = pd.DataFrame()
+    lpdf['frequency'] = filters
+    lpdf['rsquared'] = lpf
+    lpdf.to_hdf('../../Figure/lowpassfilter/LpfLWSFiltered.h5', key='lpdf', mode='w')
